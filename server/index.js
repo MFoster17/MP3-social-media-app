7a9f1d9d81e3858ce3a7dc7fcc07cb0114a23c29
MFoster17 <ausslanderofficial@gmail.com>
2023-11-08 01:39:07 -0700

    added images and started auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
  /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,8 +37,16 @@ const storage = multer.diskStorage({
   });
   const upload = multer({ storage });
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-  /* MONGOOSE SETUP */
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
